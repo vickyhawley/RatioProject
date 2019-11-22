7a2295a68eb9add4f1a39a14f1f932082462023a
Vicky Hawley <49642354+vickyhawley@users.noreply.github.com>
2019-11-22 16:31:02 +0000

    Add files via upload

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,1 +1,12 @@
-console.log('hellooooo')
+const btn = document.querySelector('#navbutton')
+const numberOfMonsters = document.querySelector('#navinput')
+
+const monsters = databaseName
+for(let i; i<Array.length; i++) {
+
+}
+
+
+function navinput(x) {
+x.style.background="black";
+}
